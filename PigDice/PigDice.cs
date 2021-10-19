e1aa4bde16038024765d99739b4b9d25023c0e70
Charlie Ford <charles.ford1978@gmail.com>
2021-10-19 16:13:46 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice
+{
+    public class PigDice
+    {
+        private Random rnd = new Random();
+        
+        public int RunToHighScore(int highScore)
+        {
+            var score = PlayPigDice();
+            var currHighScore = 0;
+            while(score < highScore)
+            {
+                score = PlayPigDice();
+                if(score > currHighScore)
+                {
+                    currHighScore = score;
+                    Console.WriteLine($"Your current highscore is {score}");
+                }
+            }
+            return score;
+         }
+        
+        public int PlayPigDice()
+        {
+            var Total = 0;
+            var roll = RollDice();
+            while(roll != 1)
+            {
+                Total += roll;
+                roll = RollDice();
+            }
+            return Total; 
+        }
+
+        private int RollDice()
+        {
+            return rnd.Next(1, 7);
+        }
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pd = new PigDice();
+            var score = pd.RunToHighScore(500);
+            Console.WriteLine($"New high score is {score}");
+            
+            //var highScore = 0;
+            //for(var idx = 0; idx < 100; idx++)
+            //{
+            //    var score = pd.PlayPigDice();
+            //    if(score > highScore)
+            //    {
+            //        highScore = score;
+            //        Console.WriteLine($"Score is {highScore}");
+                //}
+            //}
+        }
+    }
+}
